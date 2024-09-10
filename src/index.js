84fbd3a91e4895d76d608844fd0ee35937908c82
Stace113 <stace-w-13@live.co.uk>
2024-09-10 15:08:09 +0100

    installed typewriter effect using cdn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+    event.preventDefault();
 
+    new Typewriter("#poem", {
+        strings: "Hello",
+        autoStart: true,
+        delay: 75,
+        cursor: "",
+    });
+}
+
+let generatorFormElement = document.querySelector("#generator-form");
+generatorFormElement.addEventListener("submit", generatePoem);
