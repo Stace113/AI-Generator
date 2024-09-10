4b8132cd22bf3ec694e8216ba712e69851e10cd3
Stace113 <stace-w-13@live.co.uk>
2024-09-10 15:39:56 +0100

    added basic styling, HTML and added API AI integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+function displayPoem(response) {
+    console.log("poem generated");
+    new Typewriter("#poem", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 30,
+        cursor: "",
+    });
+}
+
 function generatePoem(event) {
     event.preventDefault();
 
-    new Typewriter("#poem", {
-        strings: "Hello",
-        autoStart: true,
-        delay: 75,
-        cursor: "",
-    });
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "3444d6bo8280055eta76fe9ea41a4f47";
+    let context =
+        "You are a romantic poem expert, and you love to write short poems. Your mission is to generate a poem and display as 4 lines. Make sure to follow user instructions and please be polite.";
+    let prompt = `User instructions: Generate a short 4 line poem about ${instructionsInput.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("Generating poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+    axios.get(apiUrl).then(displayPoem);
 }
 
 let generatorFormElement = document.querySelector("#generator-form");
